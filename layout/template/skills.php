6891c7b7e5fa86fd4d8bceec63bedd70aa48b3ad
Loïc Billy <loic.billy@luxroutage.lu>
2024-10-15 17:03:11 +0200

    add skills

---
+++ layout/template/skills.php
@@ -0,0 +1,9 @@
+<div id="skills--container">
+    <img src="<?=_IMG_?>plan_2.png">
+    <div class="text--container">
+        <div>Accompagnement personnalisé et adapté.</div>
+        <div>Apprentissage positif et motivant.</div>
+        <div>Enseignants qualifiés et expérimentés.</div>
+        <div>Cours flexibles à domicile.</div>
+    </div>
+</div>
