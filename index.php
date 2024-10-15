3bbce96c75734de6f238193fe13a545a01c3188a
Loïc Billy <loic.billy@luxroutage.lu>
2024-10-15 15:02:27 +0200

    add intro

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "coucou";
+
+    define('_IMG_',  '/assets/images/');
+    define('_CSS_',  '/assets/css/');
+    define('_JS_',  '/assets/js/');
+
+    include_once "./layout/header.php";
+    include_once "./layout/body.php";
+    include_once "./layout/footer.php";
+?>
+
+
+
+
+
+
+
---
+++ layout/body.php
@@ -0,0 +1,13 @@
+<?php ?>
+
+<main>
+<div id="intro--container">
+    <img src="<?=_IMG_?>plan_1.png">
+    <div class="text--container">
+        <h1>Bienvenue sur <strong>Matherapy</strong> !</h1>
+        <div>Matherapy combine mathématiques et bien-être pour aider les élèves du collège et du lycée à surmonter leurs difficultés en mathématiques.</div>
+        <div>Grâce à un accompagnement personnalisé, nous transformons les défis en réussites.</div>
+        <div> Ensemble, faisons des mathématiques une expérience positive et enrichissante !</div>
+    </div>
+</div>
+</main>
